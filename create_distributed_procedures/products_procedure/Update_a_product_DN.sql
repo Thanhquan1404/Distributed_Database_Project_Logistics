0ba02e79768f2c396d754642876e95d8946df852
Thanhquan1404 <quannguyen.14042005@gmail.com>
2026-05-31 14:53:47 +0700

    Products - procedure - Update a product - Initialize and tracking the Updated_By site

---
+++ create_distributed_procedures/products_procedure/Update_a_product_DN.sql
@@ -0,0 +1,60 @@
+----* Update product information 
+CREATE OR REPLACE PROCEDURE Update_Product(
+    v_Product_ID IN VARCHAR2,
+    v_Product_Name IN NVARCHAR2 DEFAULT NULL,
+    v_Weight IN NUMBER DEFAULT NULL,
+    v_Cost IN NUMBER DEFAULT NULL,
+    v_Rating IN NUMBER DEFAULT NULL
+)
+AS
+    v_dn_exists NUMBER;
+    v_hn_exists NUMBER;
+    v_hcm_exists NUMBER;
+BEGIN
+    -- Kiểm tra sản phẩm có tồn tại ở local và remote  không 
+    SELECT COUNT(*) INTO v_dn_exists FROM Products WHERE Product_ID = v_Product_ID;
+    SELECT COUNT(*) INTO v_hn_exists FROM Products@MinhThuy_Oracle_DistributedDatabase WHERE Product_ID = v_Product_ID;
+    SELECT COUNT(*) INTO v_hcm_exists FROM Products@KimNgan_Oracle_DistributedDatabase WHERE Product_ID = v_Product_ID;
+    
+    IF v_dn_exists = 0 THEN
+        RAISE_APPLICATION_ERROR(-20001, 'Product_ID không tồn tại ở site Đà Nẵng');
+    ELSIF v_hn_exists = 0 THEN
+        RAISE_APPLICATION_ERROR(-20001, 'Product_ID không tồn tại ở site Hà Nội');
+    ELSIF v_hcm_exists = 0 THEN
+        RAISE_APPLICATION_ERROR(-20001, 'Product_ID không tồn tại ở site Hồ Chí Minh');
+    END IF;
+
+    -- Cập nhật local
+    UPDATE Products
+    SET Product_Name = NVL(v_Product_Name, Product_Name),
+        Weight = NVL(v_Weight, Weight),
+        Cost_Of_Product = NVL(v_Cost, Cost_Of_Product),
+        Rating = NVL(v_Rating, Rating),
+        Updated_By = 'DN'
+    WHERE Product_ID = v_Product_ID;
+
+    -- Cập nhật remote HN
+    UPDATE Products@MinhThuy_Oracle_DistributedDatabase
+    SET Product_Name = NVL(v_Product_Name, Product_Name),
+        Weight = NVL(v_Weight, Weight),
+        Cost_Of_Product = NVL(v_Cost, Cost_Of_Product),
+        Rating = NVL(v_Rating, Rating),
+        Updated_By = 'DN'
+    WHERE Product_ID = v_Product_ID;
+
+    -- Cập nhật remote HCM
+    UPDATE Products@KimNgan_Oracle_DistributedDatabase
+    SET Product_Name = NVL(v_Product_Name, Product_Name),
+        Weight = NVL(v_Weight, Weight),
+        Cost_Of_Product = NVL(v_Cost, Cost_Of_Product),
+        Rating = NVL(v_Rating, Rating),
+        Updated_By = 'DN'
+    WHERE Product_ID = v_Product_ID;
+
+    COMMIT;
+EXCEPTION
+    WHEN OTHERS THEN
+        ROLLBACK;
+        RAISE;
+END Update_Product;
+/
