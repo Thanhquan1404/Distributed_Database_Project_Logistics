f2bd2b2c201c5111c8c077336d49c09eb1028157
Thanhquan1404 <quannguyen.14042005@gmail.com>
2026-05-31 17:15:03 +0700

    Customers - Insert a new customer - Initialize and tracking when inserting a new customer

---
+++ create_distributed_procedures/customers_procedure/Insert_new_customer_DN.sql
@@ -0,0 +1,54 @@
+--- Customers Procedures
+---* Function to get city in region
+CREATE OR REPLACE FUNCTION get_region_from_city(v_city NVARCHAR2) RETURN VARCHAR2
+IS
+BEGIN
+    IF v_city IN (N'Hà Nội', N'Hải Phòng', N'Bắc Ninh', N'Hạ Long', N'Nam Định', N'Thái Bình', N'Hưng Yên', N'Hải Dương') THEN
+        RETURN 'HN';
+    ELSIF v_city IN (N'Đà Nẵng', N'Huế', N'Hội An', N'Tam Kỳ', N'Quảng Ngãi', N'Bình Định', N'Phú Yên', N'Nha Trang') THEN
+        RETURN 'DN';
+    ELSIF v_city IN (N'Hồ Chí Minh', N'Cần Thơ', N'Biên Hòa', N'Vũng Tàu', N'Bình Dương', N'Long An', N'Tiền Giang', N'Đồng Nai') THEN
+        RETURN 'HCM';
+    ELSE
+        RETURN NULL; 
+    END IF;
+END;
+/
+
+---* Insert new customer
+CREATE OR REPLACE PROCEDURE Insert_Customer(
+    v_Customer_ID      VARCHAR2,
+    v_Customer_Name    NVARCHAR2,
+    v_City            NVARCHAR2,
+    v_Customer_Rating NUMBER DEFAULT 0
+) AS
+    v_region VARCHAR2(3);
+BEGIN
+    -- Xác định vùng dựa trên City
+    v_region := get_region_from_city(v_City);
+    IF v_region IS NULL THEN
+        RAISE_APPLICATION_ERROR(-20010, 'City không thuộc vùng nào được định nghĩa: ' || v_City);
+    END IF;
+
+    -- Chèn vào đúng site
+    IF v_region = 'DN' THEN
+        INSERT INTO Customers (Customer_ID, Customer_Name, City, Customer_Rating, Created_By)
+        VALUES (v_Customer_ID, v_Customer_Name, v_City, v_Customer_Rating, 'DN');
+    ELSIF v_region = 'HN' THEN
+        INSERT INTO Customers@MinhThuy_Oracle_DistributedDatabase (Customer_ID, Customer_Name, City, Customer_Rating, Created_By)
+        VALUES (v_Customer_ID, v_Customer_Name, v_City, v_Customer_Rating, 'DN');
+    ELSE -- HCM
+        INSERT INTO Customers@KimNgan_Oracle_DistributedDatabase (Customer_ID, Customer_Name, City, Customer_Rating, Created_By)
+        VALUES (v_Customer_ID, v_Customer_Name, v_City, v_Customer_Rating, 'DN');
+    END IF;
+    COMMIT;
+EXCEPTION
+    WHEN OTHERS THEN
+        ROLLBACK;
+        RAISE;
+END Insert_Customer;
+/
+
+EXEC Insert_Customer('CUS1001', 'Nguyen Van A', 'Hội An', 5);
+EXEC Insert_Customer('CUS1002', 'Le Thi B', 'Tam Kỳ', 5);
+EXEC Insert_Customer('CUS1003', 'Tran Van ', 'Quảng Ngãi', 5);
