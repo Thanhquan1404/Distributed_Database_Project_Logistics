709162050c1754349e9ba30fd3167b02befabff9
Thanhquan1404 <quannguyen.14042005@gmail.com>
2026-05-31 19:03:19 +0700

    Customers - Delete a customer - Tracking and auto delete customer in appropriate site

---
+++ create_distributed_procedures/customers_procedure/Delete_a_customer_HCM.sql
@@ -0,0 +1,28 @@
+CREATE OR REPLACE PROCEDURE Delete_Customer(v_Customer_ID VARCHAR2) AS
+    v_target_link VARCHAR2(100);
+    v_count NUMBER;
+BEGIN
+    -- Xác định site chứa customer
+    SELECT COUNT(*) INTO v_count FROM Customers WHERE Customer_ID = v_Customer_ID;
+    IF v_count > 0 THEN
+        DELETE FROM Customers WHERE Customer_ID = v_Customer_ID;
+    ELSE
+        SELECT COUNT(*) INTO v_count FROM Customers@MinhThuy_Oracle_DistributedDatabase WHERE Customer_ID = v_Customer_ID;
+        IF v_count > 0 THEN
+            DELETE FROM Customers@MinhThuy_Oracle_DistributedDatabase WHERE Customer_ID = v_Customer_ID;
+        ELSE
+            SELECT COUNT(*) INTO v_count FROM Customers@ThanhQuan_Oracle_DistributedDatabase WHERE Customer_ID = v_Customer_ID;
+            IF v_count > 0 THEN
+                DELETE FROM Customers@ThanhQuan_Oracle_DistributedDatabase WHERE Customer_ID = v_Customer_ID;
+            ELSE
+                RAISE_APPLICATION_ERROR(-20013, 'Customer_ID không tồn tại');
+            END IF;
+        END IF;
+    END IF;
+    COMMIT;
+EXCEPTION
+    WHEN OTHERS THEN
+        ROLLBACK;
+        RAISE;
+END Delete_Customer;
+/
