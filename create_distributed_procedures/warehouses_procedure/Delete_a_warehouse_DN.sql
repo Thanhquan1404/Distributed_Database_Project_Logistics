ef91bf94e40ae5c7078480475ec23e9deefb3134
Thanhquan1404 <quannguyen.14042005@gmail.com>
2026-05-31 16:23:25 +0700

    Warehouses - Delete a warehouse - Initialize procedure to delete a warehouse

---
+++ create_distributed_procedures/warehouses_procedure/Delete_a_warehouse_DN.sql
@@ -0,0 +1,24 @@
+CREATE OR REPLACE PROCEDURE Delete_Warehouse(v_Hub_ID VARCHAR2) AS
+    v_region VARCHAR2(255);
+BEGIN
+    -- Xác định region
+    BEGIN
+        SELECT Region_Code INTO v_region FROM Warehouses WHERE Hub_ID = v_Hub_ID;
+    EXCEPTION WHEN NO_DATA_FOUND THEN
+        BEGIN
+            SELECT Region_Code INTO v_region FROM Warehouses@MinhThuy_Oracle_DistributedDatabase WHERE Hub_ID = v_Hub_ID;
+        EXCEPTION WHEN NO_DATA_FOUND THEN
+            SELECT Region_Code INTO v_region FROM Warehouses@KimNgan_Oracle_DistributedDatabase WHERE Hub_ID = v_Hub_ID;
+        END;
+    END;
+    
+    IF v_region = 'DN' THEN
+        DELETE FROM Warehouses WHERE Hub_ID = v_Hub_ID;
+    ELSIF v_region = 'HN' THEN
+        DELETE FROM Warehouses@MinhThuy_Oracle_DistributedDatabase WHERE Hub_ID = v_Hub_ID;
+    ELSE
+        DELETE FROM Warehouses@KimNgan_Oracle_DistributedDatabase WHERE Hub_ID = v_Hub_ID;
+    END IF;
+    COMMIT;
+END;
+/
