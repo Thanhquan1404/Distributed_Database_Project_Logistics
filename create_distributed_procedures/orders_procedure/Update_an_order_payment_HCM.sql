0409809b7cf858630103b0a5c3f92cca150422c5
Thanhquan1404 <quannguyen.14042005@gmail.com>
2026-06-01 09:41:43 +0700

    Orders - Update order payment - Retrieve and check existence of order payment in headquarter HCM and update its info

---
+++ create_distributed_procedures/orders_procedure/Update_an_order_payment_HCM.sql
@@ -0,0 +1,24 @@
+CREATE OR REPLACE PROCEDURE Update_Order_Payment(
+    v_Order_ID   VARCHAR2,
+    v_Amount     NUMBER DEFAULT NULL,
+    v_Pay_Status VARCHAR2 DEFAULT NULL
+) AS
+    v_count NUMBER;
+BEGIN
+    -- Kiểm tra Order_Payment tồn tại tại HCM
+    SELECT COUNT(*) INTO v_count FROM Orders_Payment WHERE Order_ID = v_Order_ID;
+    IF v_count = 0 THEN
+        RAISE_APPLICATION_ERROR(-20031, 'Order_ID không tồn tại trong Orders_Payment');
+    END IF;
+
+    UPDATE Orders_Payment
+    SET Amount = NVL(v_Amount, Amount),
+        Pay_Status = NVL(v_Pay_Status, Pay_Status)
+    WHERE Order_ID = v_Order_ID;
+    COMMIT;
+EXCEPTION
+    WHEN OTHERS THEN
+        ROLLBACK;
+        RAISE;
+END Update_Order_Payment;
+/
