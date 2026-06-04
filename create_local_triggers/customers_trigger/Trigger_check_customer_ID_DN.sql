ec400548634f5c338df45867ef0feede494aab5c
Thanhquan1404 <quannguyen.14042005@gmail.com>
2026-06-04 20:38:15 +0700

    Triggers - Make confirmation for ID name of customer consistenced via sites

---
+++ create_local_triggers/customers_trigger/Trigger_check_customer_ID_DN.sql
@@ -0,0 +1,34 @@
+CREATE OR REPLACE TRIGGER TRG_CHECK_CUSTOMER_ID
+BEFORE INSERT OR UPDATE OF Customer_ID ON Customers
+FOR EACH ROW
+DECLARE
+    v_region VARCHAR2(10);
+BEGIN
+    v_region := get_region_from_city(:NEW.City);
+    
+    IF :NEW.Customer_ID NOT LIKE 'CUST' || v_region || '%' THEN
+        RAISE_APPLICATION_ERROR(-20003, 'Lỗi: Customer_ID phải bắt đầu bằng CUST + ' || v_region);
+    END IF;
+END;
+/
+
+---* Check valid ID of customer trước khi insert  
+CREATE OR REPLACE TRIGGER TRG_LOCAL_DN_CUSTOMER
+BEFORE INSERT OR UPDATE OF Customer_ID, City ON Customers
+FOR EACH ROW
+DECLARE
+    v_region VARCHAR2(10);
+BEGIN
+    v_region := get_region_from_city(:NEW.City);
+    
+    -- Kiểm tra tiền tố ID Khách hàng
+    IF :NEW.Customer_ID NOT LIKE 'CUSTDN%' THEN
+        RAISE_APPLICATION_ERROR(-20023, 'Lỗi bảo mật Site DN: Customer_ID bắt buộc phải bắt đầu bằng "CUSTDN"!');
+    END IF;
+    
+    -- Kiểm tra tính hợp lệ của phân vùng dữ liệu dựa trên Thành phố
+    IF v_region != 'DN' OR v_region IS NULL THEN
+        RAISE_APPLICATION_ERROR(-20024, 'Lỗi bảo mật Site DN: Thành phố ' || :NEW.City || ' không thuộc phạm vi quản lý của chi nhánh Đà Nẵng!');
+    END IF;
+END;
+/
---
+++ create_local_triggers/customers_trigger/Trigger_check_customer_ID_HCM.sql
@@ -0,0 +1,32 @@
+CREATE OR REPLACE TRIGGER TRG_CHECK_CUSTOMER_ID
+BEFORE INSERT OR UPDATE OF Customer_ID ON Customers
+FOR EACH ROW
+DECLARE
+    v_region VARCHAR2(10);
+BEGIN
+    v_region := get_region_from_city(:NEW.City);
+    
+    IF :NEW.Customer_ID NOT LIKE 'CUST' || v_region || '%' THEN
+        RAISE_APPLICATION_ERROR(-20003, 'Lỗi: Customer_ID phải bắt đầu bằng CUST + ' || v_region);
+    END IF;
+END;
+/
+
+---* Check valid ID of customer trước khi insert  
+CREATE OR REPLACE TRIGGER TRG_LOCAL_DN_CUSTOMER
+BEFORE INSERT OR UPDATE OF Customer_ID, City ON Customers
+FOR EACH ROW
+DECLARE
+    v_region VARCHAR2(10);
+BEGIN
+    v_region := get_region_from_city(:NEW.City);
+    
+    IF :NEW.Customer_ID NOT LIKE 'CUSTHCM%' THEN
+        RAISE_APPLICATION_ERROR(-20023, 'Lỗi bảo mật Site DN: Customer_ID bắt buộc phải bắt đầu bằng "CUSTHCM"!');
+    END IF;
+    
+    IF v_region != 'HCM' OR v_region IS NULL THEN
+        RAISE_APPLICATION_ERROR(-20024, 'Lỗi bảo mật Site DN: Thành phố ' || :NEW.City || ' không thuộc phạm vi quản lý của chi nhánh Hồ Chí Minh!');
+    END IF;
+END;
+/
